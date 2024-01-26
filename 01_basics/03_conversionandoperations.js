0992b1bc68362339daa099f5451ecdac212a1d58
Mark9-6 <98447347+Mark9-6@users.noreply.github.com>
2024-01-26 13:43:39 +0000

    datypes-summary

--- 01_basics/03_conversionandoperations.js
+++ 01_basics/03_conversionandoperations.js
@@ -1,22 +1,44 @@
-let score  = "33abc"
-// console.log(typeof score);
+// let score  = "33abc"
+// // console.log(typeof score);
 
-// console.log(typeof (score));
+// // console.log(typeof (score));
 
-// let valueinumber = Number(score)
-// console.log(typeof valueinumber);
-// console.log(valueinumber);
-// "33"=> 33
-//  "33abc"=> NaN
-//  true=> 1 ; falsev => 0
-let isloggedin = "swastik"
-let booleanIsLoggedin = Boolean(isloggedin)
-console.log(booleanIsLoggedin);
-// emty String => false
-//  1=> true 
-//  0 =>false
-// "swastik" =:> true
-let somenumber= 33
-let stringnumber = String(somenumber)
-console.log(stringnumber)
-console.log(typeof stringnumber);
+// // let valueinumber = Number(score)
+// // console.log(typeof valueinumber);
+// // console.log(valueinumber);
+// // "33"=> 33
+// //  "33abc"=> NaN
+// //  true=> 1 ; falsev => 0
+// let isloggedin = "swastik"
+// let booleanIsLoggedin = Boolean(isloggedin)
+// console.log(booleanIsLoggedin);
+// // emty String => false
+// //  1=> true 
+// //  0 =>false
+// // "swastik" =:> true
+// let somenumber= 33
+// let stringnumber = String(somenumber)
+// console.log(stringnumber)
+// console.log(typeof stringnumber);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%3);
+// let str1 = "hello"
+// let str2 = " swastik"
+// let str3 = str1+str2
+// console.log(str3);
+// console.log("1" +2); // 
+// console.log(1 +"2" +2);  // if string is first all subsequents elements are treated as string and vice versa 
+// console.log("1" +2 +2);  // here it run as preeceding from left to right 
+// console.log(1  +2+"2");  // thats why alway use bracket best practice        
+
+// console.log(+true);   // gives numeric value of true that is 1
+let num1 , num2 , num3 
+num1 = num2 = num3 = 2+2
+ let gamecounter =100
+//  gamecounter++;
+//  console.log(gamecounter)
+ ++gamecounter;
+ console.log(gamecounter)
---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,12 @@
+console.log(2>1); // gives true automatically boolean
+
+console.log("2" > 1); 
+console.log( 2 > "1"); 
+console.log(null > 0);
+console.log("null" == 0);
+console.log(null >= 0);
+console.log(undefined >= 0);
+
+// ===  checks data types of the given element 
+console.log("2"===2);
+
