4f755ec93cb24841e6b8ccfc93abbc36e461edec
Mark9-6 <98447347+Mark9-6@users.noreply.github.com>
2024-01-28 23:40:01 +0000

    foe loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,19 @@
+// for 
+// for (let i = 0; i < 10; i++) {
+//     const element =i;
+//     console.log(element);
+    
+// }
+
+let myarray = [ "flash" , "batman" , "ironman" ]
+console.log(myarray.length);
+// for(let i =0 ; i<myarray.length ; i++){
+// const element = myarray[i];
+// console.log(element);
+
+// }
+for(let i =0 ; i<myarray.length ; i++){
+const element = myarray[i];
+console.log(element);
+
+}
