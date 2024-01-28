9ab09b7fe70a58de24d9c557d37a076e6a007963
Mark9-6 <98447347+Mark9-6@users.noreply.github.com>
2024-01-28 21:45:27 +0000

    control

---
+++ 04_control_flow.js/one.js
@@ -0,0 +1,34 @@
+/// Control flow statements 
+
+// if ( 2=== "2") {
+//     console.log("executed"); // === Check whether value is ewaul as well as its type also 
+    
+// }
+// else{
+
+//     console.log("not executed "); 
+// }
+
+// const score   = 200
+// if(score  > 100){
+// const power = "fly"
+//  console.log(`user power  : ${power}`);    // var likhte hi keyword can be used outside and inside SCOPE TOO
+
+// }
+// console.log(`user power  : ${power}`);
+
+const balance = 1000
+// if(balance > 500 ) 
+//  {console.log("test")}
+// if(balance>500){
+// console.log("less than ");
+
+// }
+// else if(balance <750){
+//     console.log("less than 750");
+// }
+
+
+
+
+
