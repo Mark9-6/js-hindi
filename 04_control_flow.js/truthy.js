42c713059ab49c96e0646b9fbfa30c3ae97601bb
Mark9-6 <98447347+Mark9-6@users.noreply.github.com>
2024-01-28 23:09:52 +0000

    control flow done

---
+++ 04_control_flow.js/truthy.js
@@ -0,0 +1,28 @@
+const useremail = "swaasti@email.com"
+// if (useremail) {
+//     console.log("got an user email");
+
+// } else {
+//     console.log("dont have a user email");
+
+// }
+  // these are called truthy and falsy values  it just chekc if there is somehting inside
+  // the string or not 
+
+  // FALSY vaalues are :
+  // FALSE 
+  // ZERO , negative ZERO
+  // BigInt  ,   ""  , NaN , null , undefined 
+
+  // TRUTHY values 
+  // "0"  ,  'false' , " " , [] , {}  , function(){} 
+
+  // NULLISH COALESCING OPERATOR(??)  // this actually returns a backup value if there is no assigned value 
+  let val1 ;
+//   val1  = 5 ?? 10 
+  val1  = null  ?? 10 
+//   console.log(val1);
+
+
+  // TERNARY operator 
+//   condition ? true :false
