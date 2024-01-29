12c1507b5eb3624d6a04997fcfb055418b0814c8
Mark9-6 <98447347+Mark9-6@users.noreply.github.com>
2024-01-29 16:07:05 +0000

    high order loops

---
+++ 05_iterations/three.js
@@ -0,0 +1,12 @@
+// higher order of ArrayLoops 
+
+const  arr = [1,2,3,4,5]
+ 
+//  for (const num  of arr) {
+//     console.log(num );
+//     }
+
+const greetings = "Helloworld!"
+for(const greet of greetings ){
+    console.log(`each char is ${greet}`);
+}
