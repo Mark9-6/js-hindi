a2c6620234348ad3bb9f3484caf75b1fa62c62bf
Mark9-6 <98447347+Mark9-6@users.noreply.github.com>
2024-01-31 18:45:38 +0000

    f

---
+++ 05_iterations/four.js
@@ -0,0 +1,19 @@
+// const myobject = {
+//     js: 'javascript ',
+//     cpp: 'C++',
+//     rb: "ruby",
+//     swift: "swift by apple"
+
+
+// }
+// for(const key in myobject){
+// console.log(`${key} shortcut is for ${myobject[key]}`)
+
+// }
+
+const programming = ["js" , "cpp" , "java" , "rb" , "py"]
+
+
+for(const key in programming){
+    console.log(programming[key]);
+}
